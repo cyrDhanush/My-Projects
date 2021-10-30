f2f28d4ac6ff3bfb106cece3cf887760bff6beb6
Astitva Veer Garg <91984568+astitvaveergarg@users.noreply.github.com>
2021-10-30 09:05:17 +0530

    Add files via upload

---
+++ ImageScrapper/Image Conversion.py
@@ -0,0 +1,10 @@
+import cv2
+image = cv2.imread(input("Enter Full Path Of File: "))
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+inverted_image = 250 - gray_image
+blurred = cv2.GaussianBlur(inverted_image, (21, 21), 0)
+inverted_blurred = 255 - blurred
+pencil_sketch = cv2.divide(gray_image, inverted_blurred, scale=250.0)
+cv2.imshow("Original Image", image)
+cv2.imshow("Pencil Sketch of User Photo", pencil_sketch)
+cv2.waitKey(0)
