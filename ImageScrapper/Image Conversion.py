02cfcabc8c67b4b3300e3c4ed461595cdff5e618
astitvaveergarg <av4764@srmist.edu.in>
2021-12-07 19:27:05 +0530

    v1.0

--- ImageScrapper/Image Conversion.py
+++ ImageScrapper/Image Conversion.py
@@ -1,10 +1,11 @@
 import cv2
 image = cv2.imread(input("Enter Full Path Of File: "))
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+resixe=cv2.resize(image,(400,600))
+gray_image = cv2.cvtColor(resixe, cv2.COLOR_BGR2GRAY)
 inverted_image = 250 - gray_image
 blurred = cv2.GaussianBlur(inverted_image, (21, 21), 0)
-inverted_blurred = 255 - blurred
+inverted_blurred = 250 - blurred
 pencil_sketch = cv2.divide(gray_image, inverted_blurred, scale=250.0)
-cv2.imshow("Original Image", image)
+cv2.imshow("Original Image", resixe)
 cv2.imshow("Pencil Sketch of User Photo", pencil_sketch)
 cv2.waitKey(0)
